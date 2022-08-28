47e51872945f759f9a08dfb5a25a42e0b961ed19
YousefBawaliz <yousef2k012@gmail.com>
2022-08-28 12:28:16 +0300

    Solved

--- main.dart
+++ main.dart
@@ -1,3 +1,19 @@
 void main(){
+
+  List colors = ["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "white", "yellow"];
   
+  for (int i=0; i < colors.length; i++){
+    if(colors[i].toString().startsWith("a")||colors[i].toString().startsWith("b")){
+      var abColors= colors[i].toString().toUpperCase();
+      print("Starts with A or B: $abColors");
+    }
+
+    else{
+      int charcterLength = colors[i].toString().length;
+      print("sorry this color (${colors[i]}) doesn't start with with either a or b, has $charcterLength characters");
+      // print(colors[i]);
+    }
+
+  };
+
 }
